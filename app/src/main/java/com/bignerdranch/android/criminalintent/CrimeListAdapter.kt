8c0db250b1ef8911bc43f837554f0e468a5769f6
Jeff Vazquez <jeffvazquez@cpp.edu>
2023-09-28 09:43:02 -0700

    adding coroutine

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListAdapter.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListAdapter.kt
@@ -1,6 +1,7 @@
 package com.bignerdranch.android.criminalintent
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
@@ -20,8 +21,15 @@ class CrimeHolder(
                 Toast.LENGTH_SHORT
             ).show()
         }
+        binding.crimeSolved.visibility = if(crime.isSolved)
+        {
+            View.VISIBLE
+        } else{
+            View.GONE
+        }
     }
 }
+
 class CrimeListAdapter(
     private val crimes: List<Crime>
 ) : RecyclerView.Adapter<CrimeHolder>() {
--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListViewModel.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListViewModel.kt
@@ -1,22 +1,33 @@
 package com.bignerdranch.android.criminalintent
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import java.util.Date
 import java.util.UUID
 
+private const val TAG = "CrimeListViewModel"
 class CrimeListViewModel : ViewModel() {
 
     val crimes = mutableListOf<Crime>()
 
     init{
-        for (i in 0 until 100){
-            val crime = Crime(
-                id = UUID.randomUUID(),
-                title = "Crime #$i",
-                date = Date(),
-                isSolved = i % 2 == 0
-            )
-            crimes += crime
+        Log.d(TAG, "init starting")
+        viewModelScope.launch {
+            Log.d(TAG,"coroutine launched")
+            delay(500)
+            for (i in 0 until 100) {
+                val crime = Crime(
+                    id = UUID.randomUUID(),
+                    title = "Crime #$i",
+                    date = Date(),
+                    isSolved = i % 2 == 0
+                )
+                crimes += crime
+            }
+            Log.d(TAG,"Loading Crimes finished")
         }
     }
 }
